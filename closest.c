a4db759f7c7867e76dc25beae3b55f1c25196109
Ráfagan Abreu <rafagansa@outlook.com>
2021-10-21 03:15:37 -0300

    Teorema quadrantes 7 itens

--- closest.c
+++ closest.c
@@ -52,7 +52,7 @@ char* allocReadFileContent(const char* path) {
     return buffer;
 }
 
-InputData* allocProcessInputData(const char* input) {
+InputData allocProcessInputData(const char* input) {
     int isFirstLine = 1;
     int isReadingX = 1;
     char buffer[50] = {};
@@ -92,25 +92,23 @@ InputData* allocProcessInputData(const char* input) {
         }
     }
 
-    InputData* data = malloc(sizeof(InputData));
-    data->count = pointCount;
-    data->points = points;
+    InputData data;
+    data.count = pointCount;
+    data.points = points;
 
     return data;
 }
 
-void freeInputData(InputData* inputData) {
-    for(unsigned int i = 0; i < inputData->count; i++) {
-        free(inputData->points[i]);
+void freeInputDataPoints(InputData inputData) {
+    for(unsigned int i = 0; i < inputData.count; i++) {
+        free(inputData.points[i]);
     }
-    free(inputData);
-    inputData = NULL;
 }
 
-void logInputData(const InputData* data) {
+void logInputData(InputData data) {
     puts("Entrada :");
-    for(unsigned int i = 0; i < data->count; i++) {
-        Point* point = data->points[i];
+    for(unsigned int i = 0; i < data.count; i++) {
+        Point* point = data.points[i];
         printf("\t%d:(%f, %f)\n", i, point->x, point->y);
     }
 }
@@ -131,7 +129,7 @@ int isEqual(double a, double b) {
     return fabs(a - b) < 0.000000001;
 }
 
-OutputData findClosestPairOfPointsNaive(const InputData* input) {
+OutputData findClosestPairOfPointsNaive(InputData input) {
     Point* p1;
     Point* p2;
     double distance = DBL_MAX;
@@ -139,10 +137,10 @@ OutputData findClosestPairOfPointsNaive(const InputData* input) {
     clock_t startTime = clock();
 
     // Brute force time complexity: O(n^2)
-    for(int i = 0; i < input->count; i++) {
-        Point* _p1 = input->points[i];
-        for(int j = i + 1; j < input->count; j++) {
-            Point* _p2 = input->points[j];
+    for(int i = 0; i < input.count; i++) {
+        Point* _p1 = input.points[i];
+        for(int j = i + 1; j < input.count; j++) {
+            Point* _p2 = input.points[j];
             double d = calcEuclidianDistance(_p1, _p2);
             if(d < distance) {
                 distance = d;
@@ -169,8 +167,32 @@ int sortPointsByXAscendingComparator(const void* a, const void* b) {
    return x1 - x2;
 }
 
+int sortPointsByYAscendingComparator(const void* a, const void* b) {
+   const double y1 = (*((Point**)a))->y;
+   const double y2 = (*(Point**)b)->y;
+   return y1 - y2;
+}
+
+InputData allocFindPointsInStripeYAxisSorted(InputData input, const Point* midPoint, double delta) {
+    InputData output;
+    output.points = malloc(sizeof(Point) * input.count);
+    output.count = 0;
+    for(int i = 0; i < input.count; i++) {
+        Point* p = input.points[i];
+        if(p == midPoint) continue;
+        if(fabs(p->x - midPoint->x) < delta) {
+            output.points[output.count++] = p;
+        }
+    }
+
+    output.points = (Point**) realloc(output.points, sizeof(Point) * output.count);
+    qsort(output.points, output.count, sizeof(Point*), sortPointsByYAscendingComparator);
+    
+    return output;
+}
+
 OutputData findClosestPairOfPointsRecursive(
-    const Point** points, 
+    InputData input,
     unsigned int startIndex, 
     unsigned int endIndex
 ) {
@@ -178,28 +200,28 @@ OutputData findClosestPairOfPointsRecursive(
     const unsigned int length = endIndex - startIndex + 1;
 
     if(length == 2) {
-        output.p1 = *points[startIndex];
-        output.p2 = *points[endIndex];
+        output.p1 = *input.points[startIndex];
+        output.p2 = *input.points[endIndex];
         output.distance = calcEuclidianDistance(&output.p1, &output.p2);
     } else if(length == 3) {
         const unsigned int first = startIndex;
         const unsigned int second = startIndex + 1;
         const unsigned int third = endIndex;
         
-        const double d1 = calcEuclidianDistance(points[first], points[second]);
-        const double d2 = calcEuclidianDistance(points[first], points[third]);
-        const double d3 = calcEuclidianDistance(points[second], points[third]);
+        const double d1 = calcEuclidianDistance(input.points[first], input.points[second]);
+        const double d2 = calcEuclidianDistance(input.points[first], input.points[third]);
+        const double d3 = calcEuclidianDistance(input.points[second], input.points[third]);
         output.distance = min3(d1, d2, d3);
 
         if(isEqual(output.distance, d1)) {
-            output.p1 = *points[first];
-            output.p2 = *points[second];
+            output.p1 = *input.points[first];
+            output.p2 = *input.points[second];
         } else if(isEqual(output.distance, d2)) {
-            output.p1 = *points[first];
-            output.p2 = *points[third];
+            output.p1 = *input.points[first];
+            output.p2 = *input.points[third];
         } else {
-            output.p1 = *points[second];
-            output.p2 = *points[third];
+            output.p1 = *input.points[second];
+            output.p2 = *input.points[third];
         }
     } else {
         // Algoritmo - Parte 1:
@@ -214,45 +236,70 @@ OutputData findClosestPairOfPointsRecursive(
         const unsigned int rightStart = median;
         const unsigned int rightEnd = endIndex;
 
-        const OutputData outputLeft = findClosestPairOfPointsRecursive(points, leftStart, leftEnd);
-        const OutputData outputRight = findClosestPairOfPointsRecursive(points, rightStart, rightEnd);
+        const OutputData outputLeft = findClosestPairOfPointsRecursive(input, leftStart, leftEnd);
+        const OutputData outputRight = findClosestPairOfPointsRecursive(input, rightStart, rightEnd);
         const double delta = min2(outputLeft.distance, outputRight.distance);
 
         if(isEqual(delta, outputLeft.distance)) {
-            output.p1 = *points[leftStart];
-            output.p2 = *points[leftEnd];
+            output = outputLeft;
         } else {
-            output.p1 = *points[rightStart];
-            output.p2 = *points[rightEnd];
+            output = outputRight;
         }
-
-        output.distance = delta;
 
         // Algoritmo - Parte 2:
         // Depois de encontrar a menor distância de delta em x, procuramos entre:
-        //  [x - delta, x + delta] porque qualquer um maior que isso teria um x maior
-        //  do que a menor distância em x
+        //  [x - delta, x + delta] porque qualquer um maior que isso teria uma distancia
+        //  do que o delta encontrado
         //  Portanto, o range em x fará uma faixa que tem largura 2 * delta
-        // Quadrado 2 * delta largura por delta altura
+        // Fazendo uma análise cuidadosa, é possível perceber que em uma área
+        //  com largura 2delta e altura 2delta próxima dos botões existem no máximo 8 pontos
+        //  que podem ter uma distância menor, representando os 8 quadrantes,
+        //  4 à esquerda da borda e 4 à direita
+        // Considerando que um dos 8 pontos é o nosso encontrado, restam 7 comparações
+        // Isso faz com que essa parte do algoritmo seja O(1), e o todo seja O(nlogn)
+
+        const Point* midPoint = input.points[median];
+        InputData stripePointsData = allocFindPointsInStripeYAxisSorted(
+            input, midPoint, delta
+        );
+
+        for(int i = 0; i < stripePointsData.count; i++) {
+            Point* p1 = stripePointsData.points[i];
+            for(int j = 1; j < min2(i + 7, stripePointsData.count); j++) {
+                Point* p2 = stripePointsData.points[j];
+                if(calcEuclidianDistance(p1, p2) < delta) {
+                    output.p1 = *p1;
+                    output.p2 = *p2;
+                    output.distance = delta;
+                }
+            }
+        }
+
+        
+        free(stripePointsData.points);
     }
 
     return output;
 }
 
-OutputData findClosestPairOfPoints(const InputData* input) {
+OutputData findClosestPairOfPoints(InputData input) {
     clock_t startTime = clock();
 
-    const Point** sortedPoints = malloc(sizeof(Point) * input->count);
-    memcpy(sortedPoints, input->points, sizeof(Point) * input->count);
+    InputData sortedInputData;
+    sortedInputData.count = input.count;
+    sortedInputData.points = malloc(sizeof(Point) * input.count);
+    memcpy(sortedInputData.points, input.points, sizeof(Point) * input.count);
     
     // C std qsort (quicksort): O(nlogn)
-    qsort(sortedPoints, input->count, sizeof(Point*), sortPointsByXAscendingComparator);
+    qsort(sortedInputData.points, input.count, sizeof(Point*), sortPointsByXAscendingComparator);
 
     unsigned int startIndex = 0;
-    unsigned int endIndex = input->count - 1;
-    OutputData output = findClosestPairOfPointsRecursive(sortedPoints, 0, input->count - 1);
+    unsigned int endIndex = input.count - 1;
+    OutputData output = findClosestPairOfPointsRecursive(
+        sortedInputData, 0, input.count - 1
+    );
     
-    free(sortedPoints);
+    free(sortedInputData.points);
 
     clock_t endTime = clock();
     output.timeSecs = (double)(endTime - startTime) / CLOCKS_PER_SEC;
@@ -290,7 +337,7 @@ int main(int argc, char **argv) {
 
     char* path = argv[1];
     char* buffer = allocReadFileContent(path);
-    InputData* inputData = allocProcessInputData(buffer);
+    InputData inputData = allocProcessInputData(buffer);
     
     // printf("%s\n", buffer);
     // logInputData(inputData);
@@ -302,7 +349,6 @@ int main(int argc, char **argv) {
     logOutputData(&outputData2, 1);
 
     free(buffer);
-    freeInputData(inputData);
 
     return 0;
 }
